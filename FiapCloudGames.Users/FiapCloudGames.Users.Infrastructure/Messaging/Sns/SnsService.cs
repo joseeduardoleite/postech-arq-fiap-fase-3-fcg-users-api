f828ac57d3335a856deef8af78a742a82713810c
joseeduardoleite <jose.eduardo1201@outlook.com>
2026-03-23 21:40:21 -0300

    feat: ajusta appsettings

--- FiapCloudGames.Users/FiapCloudGames.Users.Infrastructure/Messaging/Sns/SnsService.cs
+++ FiapCloudGames.Users/FiapCloudGames.Users.Infrastructure/Messaging/Sns/SnsService.cs
@@ -2,13 +2,16 @@
 using Amazon.SimpleNotificationService.Model;
 using FiapCloudGames.Users.Domain.Messaging;
 using FiapCloudGames.Users.Domain.Messaging.Sns;
+using Microsoft.Extensions.Configuration;
 using System.Text.Json;
 
 namespace FiapCloudGames.Users.Infrastructure.Messaging.Sns;
 
-public sealed class SnsService(IAmazonSimpleNotificationService sns) : ISnsService
+public sealed class SnsService(
+    IAmazonSimpleNotificationService sns,
+    IConfiguration configuration) : ISnsService
 {
-    private readonly string _arn = "arn:aws:sns:us-east-1:252625575525:fcg-fase-3-user-created-topic";
+    private readonly string _arn = configuration["Aws:Sns:UserCreatedTopicArn"]!;
 
     public async Task PublishUserCreatedAsync(UserCreatedEvent userEvent, CancellationToken cancellationToken)
     {
